24a6825dc03cdeafe73c2a6f70ba1c9fd7fb99de
sainideepak78620 <sainideepak78620@gmail.com>
2023-04-02 19:37:04 +0530

    Styling, components state and props(defaultProps, propTypes)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react'
-import Comments from './components/Comments'
-
-const title = 'Blog Post'
-const body = 'This is my blog post'
-const comments = [
-  {id: 1, text: "Comment 1"},
-  {id: 2, text: "Comment 2"},
-  {id: 3, text: "Comment 3"}
-]
-const showComments = true
+import FeedBackItem from './components/FeedBackItem'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="container">
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-      {showComments ? <Comments comments={comments} />: <h3>No Comments</h3>}
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <FeedBackItem />
+      </div>
+    </>
   )
 }
 
