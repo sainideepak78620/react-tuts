30501629f5ca5f4fda8038142eb482ef15cff381
sainideepak78620 <sainideepak78620@gmail.com>
2023-04-02 18:23:48 +0530

    Conditional rendering in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Comments from './components/Comments'
+
+const title = 'Blog Post'
+const body = 'This is my blog post'
+const comments = [
+  {id: 1, text: "Comment 1"},
+  {id: 2, text: "Comment 2"},
+  {id: 3, text: "Comment 3"}
+]
+const showComments = true
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+      {showComments ? <Comments comments={comments} />: <h3>No Comments</h3>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
